c0bbefa5849b2a6278d22c8b79fc782485c2af87
jcpolo <jcpolo@ext.aragon.es>
2019-08-22 15:22:32 +0200

    working. As it is, just snmp configuration to devices

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from nornir.core.filter import F
 import getpass
 from nornir.core.inventory import ConnectionOptions
 import tasks
+import os
+import errno
 
 
 CFG_FILE = 'config.yaml'
@@ -11,14 +13,14 @@ PLATFORM = ['ios', 'huawei']
 EXCLUDED_VLANS = [1, 1002, 1003, 1004, 1005]
 
 
-def change_to_telnet(nr):
+def change_to_telnet(nr) -> None:
     nr.host.port = 23
     nr.host.connection_options['netmiko'] = ConnectionOptions(
         extras={"device_type": 'cisco_ios_telnet', "session_log": session_log(nr)}
     )
 
 
-def process_data_trunk(data):
+def process_data_trunk(data) -> list:
     result = []
     for interface in data:
 
@@ -39,12 +41,12 @@ def filter_inventory(nr):
         print(f'Filter by platform: { platform }')
         devices = nr.filter(F(platform=platform))
     else:
-        print(f'Platform Does not exist: { platform }.')
+        print(f'All platforms selected.')
     if site in nr.inventory.groups:
         print(f'Filter by site: { site }')
         devices = nr.filter(F(site=site))
     else:
-        print(f'Does not exist: { site } as cod_inm')
+        print(f'All sites selected.')
 
     try:
         return devices
@@ -52,14 +54,24 @@ def filter_inventory(nr):
         print(f'Error: no filter applied')
 
 
-def session_log(nr):
-    filename = f'{nr.host}-output.txt'
+def session_log(nr) -> str:
+    file = f'{nr.host}-output.txt'
+    path = './outputs/'
+    filename = f'{path}{file}'
+
+    if not os.path.exists(os.path.dirname(filename)):
+        try:
+            os.makedirs(os.path.dirname(filename))
+        except OSError as exc:  # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+
     group_object = nr.host.groups.refs[0]
     group_object.connection_options["netmiko"].extras["session_log"] = filename
     return filename
 
 
-def make_magic(nr):
+def make_magic(nr) -> None:
     # 'pasar vlan 1099 por los trunks?'
     #
     # 'crear interface capa 3 vlan 1099?'
@@ -73,18 +85,22 @@ def make_magic(nr):
     # 'agregar usuarios locales?'
     # makes a log file output for every device accessed
     session_log(nr)
-    # issue the command in the device and gets the output as a dict
-    # data = tasks.get_interfaces_status(nr)
-    # takes all trunk interfaces
-    # interfaces = process_data_trunk(data)
-    # nr.host['interfaces'] = interfaces
-
     # backup config
     tasks.backup_config(nr)
+    # issue the command in the device and gets the output as a dict
+    data = tasks.get_interfaces_status(nr)
+    # takes all trunk interfaces
+    interfaces = process_data_trunk(data)
+    nr.host['interfaces'] = interfaces
+    tasks.get_interface_description(interfaces, nr)
 
+    config(nr)
+
+
+def config(nr) -> None:
     # record configuration in the device
     template = 'snmp.j2'
-    tasks.basic_configuration(nr, template)
+    tasks.basic_configuration(template, nr)
 
 
 def main() -> None:
@@ -99,7 +115,6 @@ def main() -> None:
     devices = filter_inventory(nr)
 
     result = devices.run(task=make_magic)
-
     print_result(result)
  
  
--- tasks.py
+++ tasks.py
@@ -1,69 +1,44 @@
 from nornir.plugins.tasks import networking, text
-from nornir.core.exceptions import NornirSubTaskError
 import main
 from models import huawei, ios
 import os
 import errno
+from netmiko.ssh_exception import NetMikoTimeoutException
+import configparser
 
 
-# def get_interfaces_status(nr):
-#     r = ''
-#     if nr.host.platform == 'huawei':
-#
-#         print(f'... trying display port vlan for Huawei host: {nr.host} by SSH ...\n')
-#         r = huawei.get_interfaces_status(nr)
-#         return r
-#
-#     if nr.host.platform == 'ios':
-#
-#         print(f'... trying show interface status for Cisco host: {nr.host} by SSH ...\n')
-#         try:
-#             ssh = True
-#             r = ios.get_interfaces_status(nr)
-#
-#         except NornirSubTaskError:
-#             print('...SSH not working, closing connection attempt...')
-#             ssh = False
-#             try:
-#                 nr.host.close_connections()
-#             except ValueError:
-#                 print('...trying TELNET instead....')
-#                 pass
-#
-#         if not ssh:
-#
-#             # nr.host.connection_options['netmiko'] = ConnectionOptions(
-#             #     extras={"device_type": 'cisco_ios_telnet'})
-#
-#             main.change_to_telnet(nr)
-#
-#             print(f'Trying SHOW INTERFACE STATUS FOR {nr.host} BY TELNET')
-#             r = ios.get_interfaces_status(nr)
-#
-#     return r
+def get_ini_vars() -> dict:
+    config = configparser.RawConfigParser()
+    config.read('.global.ini')
+    r = config
+    return dict(r['GLOBAL'])
 
-def get_interfaces_status(nr):
+
+def get_interfaces_status(nr) -> list:
     r = ''
     if nr.host.platform == 'huawei':
-        print(f'... trying display port vlan for Huawei host: {nr.host}...\n')
+        # print(f'... trying display port vlan for Huawei host: {nr.host}...\n')
         r = huawei.get_interfaces_status(nr)
 
     if nr.host.platform == 'ios':
-        print(f'... trying show interface status for Cisco host: {nr.host}...\n')
+        # print(f'... trying show interface status for Cisco host: {nr.host}...\n')
         r = ios.get_interfaces_status(nr)
 
     return r
 
 
-def basic_configuration(nr, template):
+def basic_configuration(template, nr) -> None:
 
+    ini_vars = get_ini_vars()
     # Transform inventory data to configuration via a template file
     print(f'... applying config template for host: { nr.host } ...\n')
     r = nr.run(task=text.template_file,
                name=f"APLICAR PLANTILLA LOTE 7 PARA {nr.host.platform}",
                template=template,
                path=f"templates/{nr.host.platform}",
-               nr=nr)
+               nr=nr,
+               ini_vars=ini_vars,
+               )
 
     # Save the compiled configuration into a host variable
     nr.host["config"] = r.result
@@ -74,7 +49,7 @@ def basic_configuration(nr, template):
            config_commands=nr.host["config"].splitlines())
 
 
-def backup_config(nr):
+def backup_config(nr) -> None:
     r = ''
     file = f'{nr.host}-{nr.host.hostname}.cfg'
     path = './backup/'
@@ -83,19 +58,20 @@ def backup_config(nr):
     print(f'... exporting running-config for host: {nr.host} ...\n')
     if nr.host.platform == 'huawei':
 
-        print(f'... trying for Huawei host: {nr.host} by SSH ...\n')
+        # print(f'... trying for Huawei host: {nr.host} by SSH ...\n')
         r = huawei.get_config(nr)
 
     if nr.host.platform == 'ios':
 
-        print(f'... trying for Cisco host: {nr.host} by SSH ...\n')
+        # print(f'... trying for Cisco host: {nr.host} by SSH ...\n')
         try:
             ssh = True
             r = ios.get_config(nr)
 
         except:
-            print('...SSH not working, closing connection attempt...')
+            print(f'...SSH not working for {nr.host} IP {nr.host.hostname} , closing connection attempt...')
             ssh = False
+            pass
             try:
                 nr.host.close_connections()
             except ValueError:
@@ -109,8 +85,11 @@ def backup_config(nr):
 
             main.change_to_telnet(nr)
 
-            print(f'Trying FOR {nr.host} BY TELNET')
-            r = ios.get_config(nr)
+            print(f'Telnet {nr.host} IP {nr.host.hostname} ...')
+            try:
+                r = ios.get_config(nr)
+            finally:
+                print(f'Unable to connect to {nr.host} - {nr.host.hostname} by telnet\n')
 
     print(f'Saving config for {nr.host} to file {filename}')
     if not os.path.exists(os.path.dirname(filename)):
@@ -123,8 +102,28 @@ def backup_config(nr):
         f.write(r)
 
 
+def get_interface_description(interfaces, nr) -> list:
+    r = ''
+    result = []
+    for interface in interfaces:
+        if nr.host.platform == 'huawei':
+            # print(f'... trying display port vlan for Huawei host: {nr.host}...\n')
+            r = huawei.get_interface_description(interface, nr)
 
+        if nr.host.platform == 'ios':
+            # print(f'... trying show interface status for Cisco host: {nr.host}...\n')
+            r = ios.get_interface_description(interface, nr)
 
+        # do not duplicate interface name in description if it already exists
+        if interface in r[0]['description']:
+            r[0]['description'] = r[0]['description'].replace(interface, '').strip()
 
+        port_dict = {
+            'interface': interface,
+            'description': r[0]['description'],
+        }
+        result.append(port_dict)
+
+    return result
 
 
